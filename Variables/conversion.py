8cc2bdcd0341ba98f447947928443971eb5053ff
Ratul128 <mehediratul923@gmail.com>
2024-11-08 13:55:39 +0600

    python practice

---
+++ Variables/conversion.py
@@ -0,0 +1,10 @@
+#kl to mile------------------
+kl_to_mile=0.62
+result=kl_to_mile*10
+print("Kl to Mile:",result)
+
+#taka to rupee---------------
+
+rate=0.71
+conversion= rate*100
+print(f"100 Taka to Rupee:{conversion}")
---
+++ Variables/variable_demo.py
@@ -0,0 +1,14 @@
+
+print("Hello World")
+
+num1=100
+print(type(num1))
+
+num1="100"
+print(type(num1))
+
+num1=100.00
+print(type(num1))
+
+print(num1)
+
