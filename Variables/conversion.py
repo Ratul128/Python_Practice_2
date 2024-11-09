a47f9bc50fabe1c17e0f2a9fbf1477ab037b50d6
Ratul128 <mehediratul923@gmail.com>
2024-11-09 11:04:34 +0600

    conversion practice

--- Variables/conversion.py
+++ Variables/conversion.py
@@ -1,12 +1,26 @@
 #kl to mile------------------
-conv_rate=0.62
+'''conv_rate=0.62
 kilo=int(input("Enter Kl:"))
 result=conv_rate*kilo
-print("Kl to Mile:",result)
+print("Kl to Mile:",result)'''
 
 #taka to rupee---------------
 
-conv_rate=0.71
+'''conv_rate=0.71
 tk=int(input("Enter the amount(tk):"))
 result=conv_rate*tk
-print(result)
+print(f"Your Rupee is:{result}")'''
+
+#Rupee to doller convert----------
+
+'''conv_rate=0.012
+rupee=int(input("Enter the amount(rupee):"))
+result=conv_rate*rupee
+print(f"Your Doller is:{result}")'''
+
+#Taka to doller convert----------
+
+conv_rate=0.0083
+tk=int(input("Enter the amount(tk):"))
+result=conv_rate*tk
+print(f"Your Doller is:{result}")
---
+++ Variables/input_taking.py
@@ -0,0 +1,6 @@
+
+name=input("Enter Your Name:")
+email=input("Enter Your Email:")
+phone=input("Enter Your Phone:")
+
+print(f"Your Name is:{name}\nYour Email is:{email}\nYour Phone Number is:{phone}")
