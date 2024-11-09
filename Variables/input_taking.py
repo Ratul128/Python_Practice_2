af9d63e08f457a103cbda2bf89e0bce268d33581
Ratul128 <mehediratul923@gmail.com>
2024-11-09 11:19:35 +0600

    taking input from user

--- Variables/input_taking.py
+++ Variables/input_taking.py
@@ -1,6 +1,7 @@
-
+#taking input from the user-----------
 name=input("Enter Your Name:")
 email=input("Enter Your Email:")
 phone=input("Enter Your Phone:")
+
 
 print(f"Your Name is:{name}\nYour Email is:{email}\nYour Phone Number is:{phone}")
