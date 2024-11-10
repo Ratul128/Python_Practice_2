6c2798d3d04b3a7e3f8abb6cb249cf621314fc87
Ratul128 <mehediratul923@gmail.com>
2024-11-10 13:38:10 +0600

    function practice

---
+++ Function/functionDemo.py
@@ -0,0 +1,39 @@
+
+def dhaka():
+    print("Dhaka")
+
+
+def summetion():
+    num1=int(input("Enter num1:"))
+    num2=int(input("Enter num2:"))
+
+    sum=num1+num2
+    print(sum)
+
+def subtract():
+    num1=int(input("Enter num1:"))
+    num2=int(input("Enter num2:"))\
+
+    sub=num1-num2
+    print(sub)
+
+def multiply():
+    num1=int(input("Enter num1:"))
+    num2=int(input("Enter num2:"))\
+
+    mul=num1*num2
+    print(mul)
+
+def division():
+    num1=int(input("Enter num1:"))
+    num2=int(input("Enter num2:"))
+
+    div= num1/num2
+    print(div)
+
+
+summetion()
+subtract()
+multiply()
+division()
+dhaka()
