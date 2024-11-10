50dfa3875ddd1f803dd5a60196a09d8f87668bc9
Ratul128 <mehediratul923@gmail.com>
2024-11-10 22:47:14 +0600

    function practice

--- Variables/conversion.py
+++ Variables/conversion.py
@@ -1,26 +1,33 @@
 #kl to mile------------------
-'''conv_rate=0.62
-kilo=int(input("Enter Kl:"))
-result=conv_rate*kilo
-print("Kl to Mile:",result)'''
+def kl_to_mile():
+    conv_rate=0.62
+    kilo=int(input("Enter Kl:"))
+    result=conv_rate*kilo
+    print("Kl to Mile:",result)
 
 #taka to rupee---------------
-
-'''conv_rate=0.71
-tk=int(input("Enter the amount(tk):"))
-result=conv_rate*tk
-print(f"Your Rupee is:{result}")'''
+def tk_To_Rupee():
+    conv_rate=0.71
+    tk=int(input("Enter the amount(tk):"))
+    result=conv_rate*tk
+    print(f"Your Rupee is:{result}")
 
 #Rupee to doller convert----------
-
-'''conv_rate=0.012
-rupee=int(input("Enter the amount(rupee):"))
-result=conv_rate*rupee
-print(f"Your Doller is:{result}")'''
+def rupee_to_dollar():
+    conv_rate=0.012
+    rupee=int(input("Enter the amount(rupee):"))
+    result=conv_rate*rupee
+    print(f"Your Doller is:{result}")
 
 #Taka to doller convert----------
+def tk_to_doller():
+    conv_rate=0.0083
+    tk=int(input("Enter the amount(tk):"))
+    result=conv_rate*tk
+    print(f"Your Doller is:{result}")
 
-conv_rate=0.0083
-tk=int(input("Enter the amount(tk):"))
-result=conv_rate*tk
-print(f"Your Doller is:{result}")
+kl_to_mile()
+tk_To_Rupee()
+rupee_to_dollar()
+tk_to_doller()
+
