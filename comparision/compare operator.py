8833b019b058560e48c772c17643177560b77927
Ratul128 <mehediratul923@gmail.com>
2024-11-11 11:44:25 +0600

    comparision practice

---
+++ comparision/compare operator.py
@@ -0,0 +1,25 @@
+def less_than(a,b):
+    return a<b
+
+print(less_than(10,20))
+
+def grater_than(a,b):
+    return a>b
+
+print(grater_than(10,20))
+
+def greaterThan_or_equal(a,b):
+    return a>=b
+print(greaterThan_or_equal(10,10))
+
+def lessThan_or_equal(a,b):
+    return a<=b
+print(lessThan_or_equal(10,20))
+
+def equal(a,b):
+    return a==b
+print(equal(10,10 ))
+
+def not_equal(a,b):
+    return a!=b
+print(not_equal(10,10 ))
